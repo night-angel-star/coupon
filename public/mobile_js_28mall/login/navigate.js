9128e1b09a1d2f3292cc8d7c92d005bf84385e61
night-angel-star <niang00610@gmail.com>
2023-11-02 16:21:26 +0900

    changed 28mall js

--- public/mobile_js_28mall/login/navigate.js
+++ public/mobile_js_28mall/login/navigate.js
@@ -1,7 +1,12 @@
 const navigateUri = "https://28mall.com/wap/tmpl/member/email_login.html";
+const navigateToIndex = "https://28mall.com/wap";
 function navigate() {
-    window.location.href = navigateUri;
-    return navigateUri;
+    if (window.location.href === navigateToIndex) {
+        window.location.href = navigateUri;
+        return navigateUri;
+    } else {
+        window.location.href = navigateToIndex;
+    }
 }
 
 navigate();
